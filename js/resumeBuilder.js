683734f6d85c67a24845c7c89163cfd21be121c1
pumpkinjnn <jinannan@grinnell.edu>
2017-03-27 22:46:14 -0700

    append and replace exercise

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var awesomeThoughts;
+awesomeThoughts="I am Nannan and I know that I am awesome!";
+var funThoughts=awesomeThoughts.replace("awesome","fun");
+console.log(awesomeThoughts);
+console.log(funThoughts);
